88f568650989863bb8760df9d99b3d690afdf198
Greaux <quirle12@gmail.com>
2018-03-26 13:40:03 +0300

    Update Sborka.py

--- DZ2/Sborka.py
+++ DZ2/Sborka.py
@@ -1,9 +1,11 @@
+import Random
+
 class Deck(object):
     def __init__(self):
         ranks = "23456789TJQKA"
         suits = "DCHS"
         self.cards = [Card(r, s) for r in ranks for s in suits]
-        shuffle(self.cards)
+        random.shuffle(self.cards)
     def deal_card(self):
         return self.cards.pop()
 
