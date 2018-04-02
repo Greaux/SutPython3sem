1d4b743558e9f4c785610d8c2dd9ea7c6320d63c
Greaux <quirle12@gmail.com>
2018-04-02 20:20:44 +0300

    Create num of patterns.py

---
+++ Krasov/num of patterns.py
@@ -0,0 +1,16 @@
+def num_of_patterns(astr,pattern):
+    astr, pattern = astr.strip(), pattern.strip()
+    if pattern == '':return 0
+
+    ind, count, start_flag = 0,0,0
+    while True:
+        try:
+            if start_flag == 0:
+                ind = astr.index(pattern)
+                start_flag = 1
+            else:
+                ind += 1 + astr[ind+1:].index(pattern)
+            count += 1
+        except:
+            break
+    return count
